9c7d3b5888523a4db8c1a1f177a9f4dadf1eb4cc
shiv mewada <shivmewada1995@gmail.com>
2022-07-03 21:42:19 +0530

    add tst

---
+++ blog-app-apis/src/test/java/com/shiv/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiv.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

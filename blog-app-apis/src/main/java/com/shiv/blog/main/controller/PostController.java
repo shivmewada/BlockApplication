6ed977f1c9e4720d22272bf0bd6638491f6494b5
shivmewada <shivmewada@1995@gmail.com>
2022-07-20 09:44:28 +0530

    add new api download image

--- blog-app-apis/src/main/java/com/shiv/blog/main/controller/PostController.java
+++ blog-app-apis/src/main/java/com/shiv/blog/main/controller/PostController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -130,14 +132,11 @@ public class PostController {
 		return new ResponseEntity<PostDto>(updatePost,HttpStatus.OK);
 	}
 	//download image
-	@GetMapping(value = "/post/image/{imageName}",produces =MediaType.IMAGE_JPEG_VALUE)
-	public void downLoadImage(
-			@PathVariable("imageName") String imageName,
-			HttpServletResponse responce
-			)throws IOException {
-	       InputStream resources = this.fileService.getResources(path, imageName);
-	       resources
-	       
-		
+	@GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public void downLoadImage(@PathVariable("imageName") String imageName, HttpServletResponse responce)
+			throws IOException {
+		InputStream resources = this.fileService.getResources(path, imageName);
+		responce.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(resources, responce.getOutputStream());
 	}
 }
